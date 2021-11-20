4b40be5ef394e86d23688317fb2d305a06ebab8d
Aquib <shaikhaquib394@gmail.com>
2021-11-20 19:38:43 +0530

    Clean up and add documentation.

--- img2pdf.py
+++ img2pdf.py
@@ -1,29 +1,62 @@
 import os
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Image
+from reportlab.platypus import Image
 from reportlab.pdfgen import canvas
 
-x = 90
-y = 180
+def get_filenames(path) -> list:
+    """Recursively get all the filenames present from all child directories.
+    
+    Parameters
+    ----------
+    path: str
+        Directory containing all files, 
+        (files can be in sub folders)
 
-# put the paths for input directories
-paths = ['Images/2021-11-17 14-27', 
-     'Images/extractImages_a662ebda43da247612eec97a51b71b58/90 Tidewater Manor',
-     'Images/extractImages_a662ebda43da247612eec97a51b71b58/Summer Hall IG in front of house'
-     ]
+    Returns
+    -------
+    files: list
+        fullpath of all the files inside this directory and subdirectories.
+    """
+    files = []
+    for p in os.listdir(path):
+        full_path = os.path.join(path, p)
+        if os.path.isfile(full_path):
+            files.append(full_path)
+        else:
+            files += get_filenames(full_path)
+    return files
 
-for path in paths:
-    files = os.listdir(path)
+def export_img2pdf(in_path, out_path, x=90, y=80):
+    """Exports image to pdf
+    
+    Parameters
+    ----------
+    in_path: str
+        Input image path (example: Images/1.jpg).
+
+    out_path: str
+        Output pdf path (example: Data/1.pdf).
+
+    x: int  (Default=90)
+        X coordinate on the canvas.
+
+    y: int  (Default=80)
+        Y coordinate on the canvas.
+    """
+    canva = canvas.Canvas(out_path, pagesize=letter)    
+    img = Image(in_path, height=450, width=450)
+    img.drawOn(canva, x, y)
+    canva.save()
+
+if __name__ == "__main__":
+    x = 90  
+    y = 180
+
+    # directory containing images
+    img_dir = 'Images' 
+    files = get_filenames(img_dir)
+
     for f in files:
         out_fname = f.split(".")[0]+".pdf"
-
-        in_fpath = os.path.join(path, f)
-        out_fpath = os.path.join('data', out_fname)
-
-        canva = canvas.Canvas(out_fpath, pagesize=letter)    
-
-        img = Image(in_fpath, height=450, width=450)
-
-        img.drawOn(canva, x, y)
-        canva.save()
-        print(f"[+] {in_fpath} -> {out_fpath}")
+        export_img2pdf(f, out_fname, x, y)
+        print(f"[+] {f} -> {out_fname}")
